a193a4f8067e751abf93689876a243fef69693c6
CiaraRR <160429754+CiaraRR@users.noreply.github.com>
2026-07-17 21:57:43 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const CCTV_CONFIG = [
-  { id: "CAM-01", name: "N11 JUNCTION NORTH", video: "videos/cam01.mp4" },
-  { id: "CAM-02", name: "SERVICE STATION FORECOURT", video: "videos/cam02.mp4" },
-  { id: "CAM-03", name: "ESTATE ENTRANCE", video: "videos/cam03.mp4" },
-  { id: "CAM-04", name: "TOWN CENTRE EAST", video: "videos/cam04.mp4" }
-];
